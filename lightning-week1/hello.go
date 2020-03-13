0ce9f887c84133740990081ce9238ec8b42b2f0c
Adam Henley <adamazing@gmail.com>
2020-03-13 15:37:40 +1300

    Function
    Signed-off-by: Adam Henley <adamazing@gmail.com>

--- lightning-week1/hello.go
+++ lightning-week1/hello.go
@@ -7,11 +7,21 @@ import (
   "time"
 )
 
+func add(x, y int) int {
+  return x + y;
+}
+
 func main() {
   rand.Seed(time.Now().UTC().UnixNano())
   fmt.Println("Hello, cruel world.👋")
   fmt.Printf("My favourite number is %d\n", rand.Intn(5876))
   fmt.Println(math.Pi)
   fmt.Printf("Now you have %g problems. \n", math.Sqrt(7))
+
+  var x = rand.Intn(5876)
+  fmt.Printf("Doubling %d = %d\n", x, add(x,x))
+
+
+
   fmt.Println("Goodbye, cruel world.👋")
 }
