fec1cf51a12c1173cbf5c69e30d739ddb9480a2c
Adam Henley <adamazing@gmail.com>
2020-03-16 01:41:24 +1300

    Problem 1 Solution
    Signed-off-by: Adam Henley <adamazing@gmail.com>

--- project-euler/problem-0001/p001.go
+++ project-euler/problem-0001/p001.go
@@ -5,10 +5,24 @@ import (
   "time"
 )
 
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
 func main() {
   startTime := time.Now()
-  fmt.Println("Problem ")
+  fmt.Println("Problem 1")
 
+  total := 0
+  for i:=1; i<1000; i++ {
+    if i%3 == 0 {
+      total+=i
+    }else if i%5 == 0 {
+      total+=i
+    }
+  }
+  fmt.Printf("Total is %d\n", total)
 
-  fmt.Println(time.Now().Sub(startTime))
+  fmt.Printf("Took: %d",time.Now().Sub(startTime))
 }
