627e0d637d2524df543726cfd16a022a581b87eb
Adam Henley <adamazing@gmail.com>
2021-01-05 16:59:40 +1300

    Fix Problem 3
    Signed-off-by: Adam Henley <adamazing@gmail.com>

--- project-euler/problem-0003/p003.go
+++ project-euler/problem-0003/p003.go
@@ -3,8 +3,8 @@ package main
 import (
   "fmt"
   "math"
+  "math/big"
   "time"
-  "../lib/primesieve"
 )
 
 // The prime factors of 13195 are 5, 7, 13 and 29.
@@ -20,13 +20,38 @@ func largestPrimeFactor(n int,primes []int ) int {
   return n
 }
 
+// Sieve function
+//   n            The upper limit.
+//   lowerLimit   The lower limit.
+func sieve(n, lowerLimit int) []int {
+	var prime = make([]bool, n+1)
+	var primes []int
+	// fill it with true values
+	for i := range prime {
+		prime[i] = true
+	}
+	for i := 2; float64(i) <= math.Sqrt(float64(n)); i++ {
+		if big.NewInt(int64(i)).ProbablyPrime(10) {
+			for j := i * i; j <= n; j += i {
+				prime[j] = false
+			}
+		}
+	}
+	for i, v := range prime {
+		if v && i > lowerLimit {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
 func main() {
   startTime := time.Now()
   fmt.Println("Problem 3")
 
-  sieve := primesieve.Sieve(int(math.Sqrt(600851475143)),1)
+  sievedValues := sieve(int(math.Sqrt(600851475143)),1)
 
-  result := largestPrimeFactor(600851475143,sieve)
+  result := largestPrimeFactor(600851475143,sievedValues)
   fmt.Printf("Answer: %d\n",result)
 
   fmt.Printf("\nTook: %v\n", time.Now().Sub(startTime))
